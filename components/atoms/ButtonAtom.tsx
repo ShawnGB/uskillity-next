d1b7e4d02e02f88c1af38ded845a2c0a36c841bd
Shawn GB <god.shawn@gmail.com>
2023-12-01 13:24:56 +0100

    renamed button type

--- components/atoms/ButtonAtom.tsx
+++ components/atoms/ButtonAtom.tsx
@@ -1,9 +1,4 @@
-type IProps = {
-  text: string;
-  onButtonClick: () => void;
-};
-
-export default function ButtonAtom({ onButtonClick, text }: IProps) {
+export default function ButtonAtom({ onButtonClick, text }: ButtonAtomProps) {
   return (
     <button
       className='h-11 px-4 border-2 rounded-md justify-self-end'
